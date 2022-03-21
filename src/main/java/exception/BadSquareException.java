9e62163047691166e1d7f92fa156888110c12549
yslsml <mmf.antonovam@gmail.com>
2022-03-21 14:37:25 +0300

    feat: add BadSquareException class

---
+++ src/main/java/exception/BadSquareException.java
@@ -0,0 +1,22 @@
+package exception;
+
+public class BadSquareException extends Exception{
+    public BadSquareException() {
+    }
+
+    public BadSquareException(String message) {
+        super(message);
+    }
+
+    public BadSquareException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BadSquareException(Throwable cause) {
+        super(cause);
+    }
+
+    public BadSquareException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
