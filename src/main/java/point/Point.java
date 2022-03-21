97f4994daef6925835649090db533bce16b36387
yslsml <mmf.antonovam@gmail.com>
2022-03-21 13:41:42 +0300

    feat: add Point class

---
+++ src/main/java/point/Point.java
@@ -0,0 +1,68 @@
+package point;
+
+import java.util.Locale;
+import java.util.Objects;
+
+public class Point {
+    private double x;
+    private double y;
+    private String name;
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(double a) {
+        this.x = a;
+        this.y = a;
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.getX(), getX()) == 0 && Double.compare(point.getY(), getY()) == 0;
+    }
+
+    @Override
+    protected Point clone() throws CloneNotSupportedException {
+        return (Point)super.clone();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY());
+    }
+
+    @Override
+    public String toString() {
+        return name.toUpperCase(Locale.ROOT) + "(" +
+                " x= " + x +
+                ", y= " + y +
+                " )";
+    }
+}
