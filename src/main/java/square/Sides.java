2d08a36405fc04a64fe63c1060d26c55e8005be7
yslsml <mmf.antonovam@gmail.com>
2022-03-21 14:35:14 +0300

    feat: add Square class

---
+++ src/main/java/square/Sides.java
@@ -0,0 +1,90 @@
+package square;
+
+import line.Line;
+import point.Point;
+
+import java.util.Objects;
+
+public class Sides {
+    private Line a;  // top side
+    private Line b;  // right side
+    private Line c;  // bottom side
+    private Line d;  // left side
+
+    public Sides() {
+        this.a = new Line(new Point(0, 1), new Point(1,1));
+        this.b = new Line(new Point(1, 1), new Point(1,0));
+        this.c = new Line(new Point(1, 0), new Point(0,0));
+        this.d = new Line(new Point(0, 0), new Point(0,1));
+    }
+
+    public Sides(Line a, Line b, Line c, Line d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    public Line getA() {
+        return a;
+    }
+
+    public Line getB() {
+        return b;
+    }
+
+    public Line getC() {
+        return c;
+    }
+
+    public Line getD() {
+        return d;
+    }
+
+    public void setA(Line a) {
+        this.a = a;
+    }
+
+    public void setB(Line b) {
+        this.b = b;
+    }
+
+    public void setC(Line c) {
+        this.c = c;
+    }
+
+    public void setD(Line d) {
+        this.d = d;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sides sides = (Sides) o;
+        return getA().equals(sides.getA()) &&
+                getB().equals(sides.getB()) &&
+                getC().equals(sides.getC()) &&
+                getD().equals(sides.getD());
+    }
+
+    @Override
+    protected Sides clone() throws CloneNotSupportedException {
+        Sides copy = null;
+        try {
+            copy = (Sides) super.clone();
+            copy.a = (Line) a.clone();
+            copy.b = (Line) b.clone();
+            copy.c = (Line) c.clone();
+            copy.d = (Line) d.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return copy;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getA(), getB(), getC(), getD());
+    }
+}
---
+++ src/main/java/square/Square.java
@@ -0,0 +1,82 @@
+package square;
+
+import line.Line;
+import point.Point;
+import valid.ValidSquare;
+
+import java.util.Objects;
+
+//Методы: задание размеров, растяжение, сжатие, поворот, изменение цвета
+
+public class Square {
+
+    private Sides sides;
+    private Color color;
+
+    public Square() {
+        sides = new Sides();
+        color = Color.BLACK;
+    }
+
+    public Square(Sides sides) {
+        if (ValidSquare.isValidSquare(sides)) {
+            this.sides = sides;
+            color = Color.BLACK;
+        }
+        else throw new BadSquareException("Square sides are incorrect!");
+    }
+
+    public Square(Sides sides, Color color) {
+        this(sides);
+        this.color = color;
+    }
+
+    public Sides getSides() {
+        return sides;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setSides(Sides sides) {
+        this.sides = sides;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    protected Square clone() throws CloneNotSupportedException {  // добавить
+        Square copy = null;
+        try {
+            copy = (Square) super.clone();
+            copy.sides = (Sides) sides.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return copy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Square square = (Square) o;
+        return getSides().equals(square.getSides()) && getColor() == square.getColor();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getSides(), getColor());
+    }
+
+    @Override
+    public String toString() {
+        return "Square {" +
+                "sides=" + sides +
+                ", color=" + color +
+                '}';
+    }
+}
