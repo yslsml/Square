68f30c0742a8489d9059f7b9d322e28cd7a05d41
yslsml <mmf.antonovam@gmail.com>
2022-03-22 21:10:26 +0300

    feat: add setName and getName methods to Point class

--- src/main/java/point/Point.java
+++ src/main/java/point/Point.java
@@ -32,12 +32,20 @@ public class Point {
         return y;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setX(double x) {
         this.x = x;
     }
 
     public void setY(double y) {
         this.y = y;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
