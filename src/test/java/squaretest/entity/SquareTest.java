bacc02d065e34efbcc70f5fdf1bbba80723765cf
yslsml <mmf.antonovam@gmail.com>
2022-03-25 10:28:35 +0300

    feat: add tests for Square class

---
+++ src/test/java/squaretest/entity/SquareTest.java
@@ -0,0 +1,49 @@
+package squaretest.entity;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import square.entity.*;
+import square.exception.BadSquareException;
+
+public class SquareTest {
+
+    @BeforeMethod
+    public Square setUp() throws BadSquareException {
+        Line a = new Line(new Point(1,4), new Point(4,4));
+        Line b = new Line(new Point(4,4), new Point(4,1));
+        Line c = new Line(new Point(4,1), new Point(1,1));
+        Line d = new Line(new Point(1,1), new Point(1,4));
+        Sides sides = new Sides(a,b,c,d);
+        Square sq = new Square(sides, Color.BLACK);
+        return sq;
+    }
+
+    @Test
+    public void testSetColor() throws BadSquareException {
+        Square sq = setUp();
+        sq.setColor(Color.RED);
+        Color actual = sq.getColor();
+        Color expected = Color.RED;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testSetSize() throws BadSquareException {
+        Square actual = setUp();
+        actual.setSize(6);
+        Square expected = new Square(new Point(1,4), new Point(7,4), new Point(7,-2), new Point(1,-2));
+        Assert.assertEquals(actual, expected);
+    }
+
+    /*@Test
+    public void testRotate() throws BadSquareException {
+        Square actual = setUp();
+        actual.rotate(Math.PI/3);
+        Square expected = new Square(new Point(1,4), new Point(2.5, 1.59),
+                          new Point(-0.098, 0.09), new Point(-1.598,2.5));
+        Assert.assertEquals(actual, expected);
+    }*/
+
+}
